17bde37bf30365f2eac4813864ca197c5e0d7b72
badr jraou <130531455+badrjr1@users.noreply.github.com>
2026-03-27 01:34:54 +0100

    Change browser launch to headless mode

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -16,7 +16,7 @@ public class Hooks {
     @Before
     public void setUp() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
         page = browser.newPage();
     }
 
